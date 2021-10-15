6c32096f89345fbf02c177ba9400eaeb14b06d39
Kyriakos Bellios <kyr@designisdesign.eu>
2021-10-15 13:58:24 +0300

    Fixed formula covertagger.rb
    svn: E230001: Server SSL certificate verification failed: certificate
    has expired

--- covertagger.rb
+++ covertagger.rb
@@ -1,7 +1,7 @@
 class Covertagger < Formula
   desc "This is a simple application that will try to find a album art image (png or jpg) and add it directly into the metadata of all your mp3 files"
   homepage 'http://www.musaraigne.net/covertagger'
-  head 'https://svn.code.sf.net/p/covertagger/code/'
+  head 'http://svn.code.sf.net/p/covertagger/code/'
   version '1.0.1'
 
   depends_on 'taglib' #=> :build
